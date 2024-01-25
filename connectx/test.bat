8bee07d745a3bd816b73ecc2eef81fe7f44c1e27
Alice Benatti <alice17bee@gmail.com>
2024-01-25 21:24:05 +0100

    chore: fix verbose mode output

--- connectx/test.bat
+++ connectx/test.bat
@@ -32,7 +32,7 @@ set /a error1=0
 
 ::Bob2 Score: 18 Won: 5 Lost: 0 Draw: 3 Error: 2
 ::L1 Score: 9 Won: 0 Lost: 5 Draw: 3 Error: 0
-REM Read from the input file
+REM Read from the input file "board_configuration.txt"
 for /f "delims=" %%a in (%input%) do (
 	echo.
 	echo [36mTesting %%a[0m
@@ -40,9 +40,6 @@ for /f "delims=" %%a in (%input%) do (
 	echo|set /p= "- !PLAYER0! vs !PLAYER1! - "
 	FOR /F "tokens=1,3,5,7,9,11 delims=: " %%F IN ('java -cp ".." connectx.CXPlayerTester %%a connectx.!PLAYER0!.!PLAYER0! connectx.!PLAYER1!.!PLAYER1! -r 1 -t 10') DO (
 		:: F = Player, G = Score, H = Won, I = Lost, J = Draw, K = Error
-		if "!verbose!"=="true" (
-			echo %%F 
-		)
         if "%%F" == "!PLAYER0!" (
             set /a score0=!score0!+%%G
             set /a won0=!won0!+%%H
@@ -66,37 +63,41 @@ for /f "delims=" %%a in (%input%) do (
             set /a draw1=!draw1!+%%J
             set /a error1=!error1!+%%K
 		)
+        if "!verbose!"=="true" (
+            echo %%F Score: %%G, Won: %%H, Lost: %%I, Draw: %%J, Error: %%K
+        )
 	)
 	REM Run the game PLAYER1! vs PLAYER0
+    echo.
 	echo|set /p= "- !PLAYER1! vs !PLAYER0! - "
 	FOR /F "tokens=1,3,5,7,9,11 delims=: " %%F IN ('java -cp ".." connectx.CXPlayerTester %%a connectx.!PLAYER1!.!PLAYER1! connectx.!PLAYER0!.!PLAYER0! -r 1 -t 10') DO (
 		:: F = Player, G = Score, H = Won, I = Lost, J = Draw, K = Error
-		if "!verbose!"=="true" (
-			echo %%F 
-		)
-        if "%%F" == "!PLAYER0!" (
+        if "%%F" == "!PLAYER1!" (
             set /a score0=!score0!+%%G
             set /a won0=!won0!+%%H
             set /a lost0=!lost0!+%%I
             set /a draw0=!draw0!+%%J
             set /a error0=!error0!+%%K
             if %%H == 1 (
-                echo [93m!PLAYER0! WIN[0m
+                echo [91m!PLAYER1! WIN[0m
             ) else if %%J == 1 (
                 echo [92mDRAW[0m
             ) else if %%I == 1 (
-                echo [91m!PLAYER1! WIN[0m
+                echo [93m!PLAYER0! WIN[0m
             ) else (
                 echo [95mNot assigned[0m
             )
-        )
-        if "%%F" == "!PLAYER1!" (
+        )        
+        if "%%F" == "!PLAYER0!" (
             set /a score1=!score1!+%%G
             set /a won1=!won1!+%%H
             set /a lost1=!lost1!+%%I
             set /a draw1=!draw1!+%%J
             set /a error1=!error1!+%%K
 		)
+        if "!verbose!"=="true" (
+            echo %%F Score: %%G, Won: %%H, Lost: %%I, Draw: %%J, Error: %%K
+        )
 	)
 )
 REM Print the final result
