ed8e31f5bf0bd5b37cc944d7a78371950fa30a60
Alice Benatti <alice17bee@gmail.com>
2024-02-03 10:59:08 +0100

    new analysis file & script

--- connectx/hundredplay.bat
+++ connectx/hundredplay.bat
@@ -32,8 +32,7 @@ for /f "delims=" %%a in (%input%) do (
 	echo.
 	echo [36mTesting %%a[0m
 	REM Run the game PLAYER0 vs PLAYER1
-	echo|set /p= "- !PLAYER0! vs !PLAYER1! - "
-	FOR /F "tokens=1,3,5,7,9,11 delims=: " %%F IN ('java -cp ".." connectx.CXPlayerTester %%a connectx.!PLAYER0!.!PLAYER0! connectx.!PLAYER1!.!PLAYER1! -r 10 -t 10') DO (
+	FOR /F "tokens=1,3,5,7,9,11 delims=: " %%F IN ('java -cp ".." connectx.CXPlayerTester %%a connectx.!PLAYER0!.!PLAYER0! connectx.!PLAYER1!.!PLAYER1! -r 1 -t 10') DO (
 		:: F = Player, G = Score, H = Won, I = Lost, J = Draw, K = Error
         if "%%F" == "!PLAYER0!" (
             set /a score0=!score0!+%%G
@@ -41,14 +40,16 @@ for /f "delims=" %%a in (%input%) do (
             set /a lost0=!lost0!+%%I
             set /a draw0=!draw0!+%%J
             set /a error0=!error0!+%%K
-            if %%H == 1 (
-                echo [93m!PLAYER0! WIN[0m
-            ) else if %%J == 1 (
-                echo [92mDRAW[0m
-            ) else if %%I == 1 (
-                echo [91m!PLAYER1! WIN[0m
-            ) else (
-                echo [95mNot assigned[0m
+            if "!verbose!" == "true" (
+                if %%H == 1 (
+                    echo [93m!PLAYER0! WIN[0m
+                ) else if %%J == 1 (
+                    echo [92mDRAW[0m
+                ) else if %%I == 1 (
+                    echo [91m!PLAYER1! WIN[0m
+                ) else (
+                    echo [95mNot assigned[0m
+                )
             )
         )
         if "%%F" == "!PLAYER1!" (
@@ -62,22 +63,12 @@ for /f "delims=" %%a in (%input%) do (
             echo %%F Score: %%G, Won: %%H, Lost: %%I, Draw: %%J, Error: %%K
         )
 	)
-
-    REM Print the final result
+)
 echo ==============================
-echo [92mFinal results:[0m
-echo !PLAYER0!	 Score: !score0!, Won: !won0!, Lost: !lost0!, Draw: !draw0!, Error: !error0! 
-echo !PLAYER1!	 Score: !score1!, Won: !won1!, Lost: !lost1!, Draw: !draw1!, Error: !error1! 
-
-echo ============================== >> hundredResults.txt
-echo !PLAYER0! vs !PLAYER1! Final results: >> hundredResults.txt
-echo !PLAYER0!	 Score: !score0!, Won: !won0!, Lost: !lost0!, Draw: !draw0!, Error: !error0!  >> hundredResults.txt
-echo !PLAYER1!	 Score: !score1!, Won: !won1!, Lost: !lost1!, Draw: !draw1!, Error: !error1!  >> hundredResults.txt
-
+for /f "delims=" %%a in (%input%) do (
 	REM Run the game PLAYER1! vs PLAYER0
-    echo.
-	echo|set /p= "- !PLAYER1! vs !PLAYER0! - "
-	FOR /F "tokens=1,3,5,7,9,11 delims=: " %%F IN ('java -cp ".." connectx.CXPlayerTester %%a connectx.!PLAYER1!.!PLAYER1! connectx.!PLAYER0!.!PLAYER0! -r 10 -t 10') DO (
+	echo [36mTesting %%a[0m
+	FOR /F "tokens=1,3,5,7,9,11 delims=: " %%F IN ('java -cp ".." connectx.CXPlayerTester %%a connectx.!PLAYER1!.!PLAYER1! connectx.!PLAYER0!.!PLAYER0! -r 1 -t 10') DO (
 		:: F = Player, G = Score, H = Won, I = Lost, J = Draw, K = Error
         if "%%F" == "!PLAYER1!" (
             set /a score1=!score1!+%%G
@@ -85,14 +76,16 @@ echo !PLAYER1!	 Score: !score1!, Won: !won1!, Lost: !lost1!, Draw: !draw1!, Erro
             set /a lost1=!lost1!+%%I
             set /a draw1=!draw1!+%%J
             set /a error1=!error1!+%%K
-            if %%H == 1 (
-                echo [91m!PLAYER1! WIN[0m
-            ) else if %%J == 1 (
-                echo [92mDRAW[0m
-            ) else if %%I == 1 (
-                echo [93m!PLAYER0! WIN[0m
-            ) else (
-                echo [95mNot assigned[0m
+            if "!verbose!" == "true" (
+                if %%H == 1 (
+                    echo [91m!PLAYER1! WIN[0m
+                ) else if %%J == 1 (
+                    echo [92mDRAW[0m
+                ) else if %%I == 1 (
+                    echo [93m!PLAYER0! WIN[0m
+                ) else (
+                    echo [95mNot assigned[0m
+                )
             )
 		)
         if "%%F" == "!PLAYER0!" (
@@ -112,10 +105,6 @@ echo ==============================
 echo [92mFinal results:[0m
 echo !PLAYER0!	 Score: !score0!, Won: !won0!, Lost: !lost0!, Draw: !draw0!, Error: !error0! 
 echo !PLAYER1!	 Score: !score1!, Won: !won1!, Lost: !lost1!, Draw: !draw1!, Error: !error1! 
-
-::bell sound for hearing end of batch file
-@echo off
-PowerShell -Command "Write-Host `a"
 
 echo ============================== >> hundredResults.txt
 echo !PLAYER1! vs !PLAYER0! Final results: >> hundredResults.txt
--- connectx/startmegatest.bat
+++ connectx/startmegatest.bat
@@ -8,3 +8,8 @@ for /f "delims=" %%a in (%players%) do (
 	hundredplay.bat L0 %%a f
 	hundredplay.bat L1 %%a f
 )
+
+
+::bell sound for hearing end of batch file
+@echo off
+PowerShell -Command "Write-Host `a"
